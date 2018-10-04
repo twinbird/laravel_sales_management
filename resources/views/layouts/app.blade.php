7a51912098bcd76185fb4eb821201ad7e13cb1d9
twinbird <ixa2063@gmail.com>
2018-10-04 07:33:37 +0000

    レイアウトのナビゲーションバーを変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,21 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+						<li class="nav-item">
+							<a class="nav-link">ホーム</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link">見積</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link">顧客</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link">商品</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link">設定</a>
+						</li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
