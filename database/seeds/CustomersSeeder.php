8063e21c3178c0b2365d1fdc3dd8f8c19238ffbd
twinbird <ixa2063@gmail.com>
2018-10-05 06:00:09 +0000

    seedを作成

---
+++ database/seeds/CustomersSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CustomersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		// testユーザに対するダミー顧客
+		App\Customer::create([
+			'user_id' => App\User::where('email', 'test@example.com')->first()->id,
+			'name' => '株式会社Laravel',
+			'address1' => '東京都港区',
+			'address2' => 'xxx-yyy',
+			'tel' => '03-1234-5678',
+			'fax' => '03-9876-5432',
+			'payment_term' => '月末締め翌々月末払い',
+		]);
+		App\Customer::create([
+			'user_id' => App\User::where('email', 'test@example.com')->first()->id,
+			'name' => 'ララーベル株式会社',
+			'address1' => '石川県金沢市',
+			'address2' => 'xxx-yyy',
+			'tel' => '090-1234-5678',
+			'fax' => '090-9876-5432',
+			'payment_term' => '月末締め翌月15日払い',
+		]);
+    }
+}
