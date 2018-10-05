b1a5454b8a77e776394ae1cbc551a05855706040
twinbird <ixa2063@gmail.com>
2018-10-05 07:38:47 +0000

    リスティングページを作成

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
 
 class CustomersController extends Controller
@@ -13,7 +14,11 @@ class CustomersController extends Controller
      */
     public function index()
     {
-		return view('customers.index');
+		$customers = Customer::paginate(15);
+		$params = [
+			'customers' => $customers,
+		];
+		return view('customers.index', $params);
     }
 
     /**
--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -7,6 +7,22 @@
 	<a class="btn btn-primary float-right" href="{{ url('/customers/create') }}">新しい顧客を登録</a>
 
 	<table class="table">
+		<tr>
+			<th>顧客名</th>
+			<th>住所</th>
+			<th>TEL</th>
+			<th>FAX</th>
+		</tr>
+		@foreach($customers as $customer)
+		<tr>
+			<td>{{ $customer->name }}</td>
+			<td>{{ $customer->address1 }}</td>
+			<td>{{ $customer->tel }}</td>
+			<td>{{ $customer->fax }}</td>
+		</tr>
+		@endforeach
 	</table>
+
+	{{ $customers->links() }}
 </div>
 @endsection
