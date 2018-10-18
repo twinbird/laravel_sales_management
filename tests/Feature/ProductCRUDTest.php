522b1defd0ca01921c38066058eddfaae0160608
twinbird <ixa2063@gmail.com>
2018-10-18 07:53:55 +0000

    商品ページ全てのテストを追加

--- tests/Feature/ProductCRUDTest.php
+++ tests/Feature/ProductCRUDTest.php
@@ -10,13 +10,16 @@ use App\Product;
 
 class ProductCRUDTest extends TestCase
 {
+	use RefreshDatabase;
+
 	protected $user;
 	protected $product;
 
 	public function setUp()
 	{
 		parent::setUp();
-		$this->user = factory(User::class)->create();
+		$this->artisan('db:seed');
+		$this->user = User::where('email', 'test@example.com')->first();
 		$this->product = factory(Product::class)->create(['user_id' => $this->user->id]);
 	}
 
@@ -26,11 +29,37 @@ class ProductCRUDTest extends TestCase
 		$response->assertRedirect('/login');
     }
 
-	public function testListingProducts()
+	public function testListingProductsPage()
 	{
 		$response = $this->actingAs($this->user)
 						 ->get('/products');
 		$response->assertOk();
+		$response->assertSee('新しい商品の登録', 'a');
+	}
+
+	public function testSeeProductNameOnListingPage()
+	{
+		$response = $this->actingAs($this->user)
+							->get('/products');
+		$response->assertOk();
+		$first_product = $this->user->products->sortBy('name')->take(1)->first();
+		$response->assertSee($first_product->name);
+	}
+
+	public function testSeeEditButtonOnListingPage()
+	{
+		$response = $this->actingAs($this->user)
+							->get('/products');
+		$response->assertOk();
+		$response->assertSee('編集');
+	}
+
+	public function testSeeDeleteButtonOnListingPage()
+	{
+		$response = $this->actingAs($this->user)
+							->get('/products');
+		$response->assertOk();
+		$response->assertSee('削除');
 	}
 
 	public function testCreateProductPage()
@@ -38,6 +67,7 @@ class ProductCRUDTest extends TestCase
 		$response = $this->actingAs($this->user)
 						 ->get('/products/create');
 		$response->assertOk();
+		$response->assertSee('登録');
 	}
 
 	public function testEditProductPage()
@@ -45,5 +75,14 @@ class ProductCRUDTest extends TestCase
 		$response = $this->actingAs($this->user)
 							->get(route('products.edit', ['id' => $this->product->id]));
 		$response->assertOk();
+		$response->assertSee('更新');
+	}
+
+	public function testShowProductPage()
+	{
+		$response = $this->actingAs($this->user)
+							->get(route('products.show', ['id' => $this->product->id]));
+		$response->assertOk();
+		$response->assertSee($this->product->name);
 	}
 }
