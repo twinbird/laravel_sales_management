b036dac60aa6cb0f7774d57f89e98b8fa64dc0aa
twinbird <ixa2063@gmail.com>
2018-10-21 20:29:35 +0000

    未認証時にメニューバーを表示しないように変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,7 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+					@if (Auth::check())
                     <ul class="navbar-nav mr-auto">
 						<li class="nav-item">
 							<a class="nav-link">ホーム</a>
@@ -49,6 +50,7 @@
 							<a class="nav-link" href="{{ route('profiles.edit', ['id' => Auth::user()->profile->id]) }}">設定</a>
 						</li>
                     </ul>
+					@endif
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
