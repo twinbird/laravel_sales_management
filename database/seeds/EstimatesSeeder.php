e7e26caed53354a55c852daf626b5d02c909b0ec
twinbird <ixa2063@gmail.com>
2018-10-22 08:14:04 +0000

    顧客IDを見積テーブルへ追加

--- database/seeds/EstimatesSeeder.php
+++ database/seeds/EstimatesSeeder.php
@@ -12,9 +12,12 @@ class EstimatesSeeder extends Seeder
     public function run()
     {
         // 見積を20作成
-		$other_user_id = App\User::where('email', 'other@example.com')->first()->id;
+		$other_user = App\User::where('email', 'other@example.com')->first();
+		$other_user_id = $other_user->id;
+		$other_user_customer_id = $other_user->withoutGlobalScopes()->first()->id;
 		factory(App\Estimate::class, 20)->create([
 			'user_id' => $other_user_id,
+			'customer_id' => $other_user_customer_id,
 		])->each(function($estimate) use ($other_user_id) {
 			factory(App\EstimateDetail::class, 5)->create([
 				'user_id' => $other_user_id,
