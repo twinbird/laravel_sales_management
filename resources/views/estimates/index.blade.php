0b86cd2a14c05a9cf4705e0a6d6f68aea858232a
twinbird <ixa2063@gmail.com>
2018-11-01 21:06:33 +0000

    ボタンなどを全体的に小さく変更した

--- resources/views/estimates/index.blade.php
+++ resources/views/estimates/index.blade.php
@@ -7,10 +7,10 @@
 	<h2>見積の管理</h2>
 
 	<div class="row float-right">
-		<a href="{{ route('estimates.create') }}" class="btn btn-primary mb-2">新しい見積の作成</a>
+		<a href="{{ route('estimates.create') }}" class="btn btn-primary mb-2 btn-sm">新しい見積の作成</a>
 	</div>
 
-	<table class="table">
+	<table class="table table-sm">
 		<tr>
 			<th>顧客名</th>
 			<th>件名</th>
@@ -25,12 +25,12 @@
 			<td><a href="{{ route('estimates.show', ['id' => $estimate->id]) }}">{{ $estimate->title }}</a></td>
 			<td>{{ $estimate->total_price }}</td>
 			<td>{{ $estimate->issue_date->format('Y/m/d') }}</td>
-			<td><a href="{{ route('estimates.edit', ['id' => $estimate->id]) }}" class="btn btn-info">編集</a></td>
+			<td><a href="{{ route('estimates.edit', ['id' => $estimate->id]) }}" class="btn btn-info btn-sm">編集</a></td>
 			<td>
 				<form method="POST" action="{{ route('estimates.destroy', ['id' => $estimate->id]) }}">
 					@csrf
 					@method('delete')
-					<input type="submit" value="削除" onClick="return confirm('削除してよろしいですか?')" class="btn btn-danger">
+					<input type="submit" value="削除" onClick="return confirm('削除してよろしいですか?')" class="btn btn-danger btn-sm">
 				</form>
 			</td>
 		</tr>
