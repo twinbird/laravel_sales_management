ea14bb4c3fda881fc1c69483c1064ba4a04502e8
twinbird <ixa2063@gmail.com>
2018-11-09 07:30:45 +0000

    見積書に値を埋め込むようにした

--- resources/views/estimates/report.blade.php
+++ resources/views/estimates/report.blade.php
@@ -18,28 +18,27 @@
     <div id="header-container">
       <div id="left-block">
         <div id="left-block-strong-area">
-          <div id="customer-name-block"><span id="customer-name">テスト顧客1</span>御中</div>
-          <div id="title">件名:案件1</div>
+		  <div id="customer-name-block"><span id="customer-name">{{ $estimate->customer_name }}</span>御中</div>
+          <div id="title">件名: {{ $estimate->title }}</div>
           <div id="greeting-message">下記の通り、御見積申し上げます。</div>
         </div>
   
-        <div class="left-block-elements">納期:</div>
-        <div class="left-block-elements">支払条件:月末締め翌月25日払い</div>
-        <div class="left-block-elements">見積有効期限:2018/11/17</div>
-        <div id="total-price">御見積金額:<span id="total-price-num" class="left-block-elements">5,000.0円</span></div>
+        <div class="left-block-elements">納期: {{ $estimate->due_date }}</div>
+        <div class="left-block-elements">支払条件: {{ $estimate->payment_term }}</div>
+        <div class="left-block-elements">見積有効期限: {{ $estimate->effective_date }}</div>
+        <div id="total-price">御見積金額:<span id="total-price-num" class="left-block-elements">{{ $estimate->total_price }} 円</span></div>
       </div>
   
       <div id="right-block">
-        <div class="right-block-elements">見積番号: 00000000000001</div>
-        <div class="right-block-elements">発行日: 2018/11/09</div>
+        <div class="right-block-elements">見積番号: {{ $estimate->estimate_no }}</div>
+        <div class="right-block-elements">発行日: {{ $estimate->issue_date }}</div>
     
-        <div class="right-block-elements">test</div>
-        <div class="right-block-elements">〒</div>
-        <div class="right-block-elements"></div>
-        <div class="right-block-elements"></div>
-        <div class="right-block-elements">Email:</div>
-        <div class="right-block-elements">TEL:</div>
-        <div class="right-block-elements">FAX:</div>
+        <div class="right-block-elements">{{ $estimate->self_company_name}}</div>
+        <div class="right-block-elements">〒 {{ $estimate->self_postal_code }}</div>
+        <div class="right-block-elements">{{ $estimate->self_address1 }}</div>
+        <div class="right-block-elements">{{ $estimate->self_address2 }}</div>
+        <div class="right-block-elements">TEL: {{ $estimate->self_tel }}</div>
+        <div class="right-block-elements">FAX: {{ $estimate->self_fax }}</div>
       </div>
     </div>
 
@@ -55,20 +54,17 @@
           </tr>
         </thead>
         <tbody>
+		@foreach ($estimate->estimate_details as $detail)
           <tr>
-            <td class="detail-no">1</td>
-            <td class="detail-item-name">サンプルA</td>
-            <td class="detail-quantity">10.0</td>
-            <td class="detail-unit-price">100.0</td>
-            <td class="detail-price">1,000.0</td>
+            <td class="detail-no">{{ $loop->iteration }}</td>
+            <td class="detail-item-name">{{ $detail->item_name }}</td>
+            <td class="detail-quantity">{{ $detail->quantity }}</td>
+            <td class="detail-unit-price">{{ $detail->unit_price }}</td>
+            <td class="detail-price">{{ $detail->price }}</td>
           </tr>
-          <tr>
-            <td class="detail-no">2</td>
-            <td class="detail-item-name">サンプルB</td>
-            <td class="detail-quantity">20.0</td>
-            <td class="detail-unit-price">200.0</td>
-            <td class="detail-price">4,000.0</td>
-          </tr>
+		@endforeach
+		{{-- 最低10明細は作っておく --}}
+		@for ($i = $estimate->estimate_details->count(); $i < 10; $i++)
           <tr>
             <td class="detail-no"></td>
             <td class="detail-item-name"></td>
@@ -76,62 +72,14 @@
             <td class="detail-unit-price"></td>
             <td class="detail-price"></td>
           </tr>
-          <tr>
-            <td class="detail-no"></td>
-            <td class="detail-item-name"></td>
-            <td class="detail-quantity"></td>
-            <td class="detail-unit-price"></td>
-            <td class="detail-price"></td>
-          </tr>
-          <tr>
-            <td class="detail-no"></td>
-            <td class="detail-item-name"></td>
-            <td class="detail-quantity"></td>
-            <td class="detail-unit-price"></td>
-            <td class="detail-price"></td>
-          </tr>
-          <tr>
-            <td class="detail-no"></td>
-            <td class="detail-item-name"></td>
-            <td class="detail-quantity"></td>
-            <td class="detail-unit-price"></td>
-            <td class="detail-price"></td>
-          </tr>
-          <tr>
-            <td class="detail-no"></td>
-            <td class="detail-item-name"></td>
-            <td class="detail-quantity"></td>
-            <td class="detail-unit-price"></td>
-            <td class="detail-price"></td>
-          </tr>
-          <tr>
-            <td class="detail-no"></td>
-            <td class="detail-item-name"></td>
-            <td class="detail-quantity"></td>
-            <td class="detail-unit-price"></td>
-            <td class="detail-price"></td>
-          </tr>
-          <tr>
-            <td class="detail-no"></td>
-            <td class="detail-item-name"></td>
-            <td class="detail-quantity"></td>
-            <td class="detail-unit-price"></td>
-            <td class="detail-price"></td>
-          </tr>
-          <tr>
-            <td class="detail-no"></td>
-            <td class="detail-item-name"></td>
-            <td class="detail-quantity"></td>
-            <td class="detail-unit-price"></td>
-            <td class="detail-price"></td>
-          </tr>
+		@endfor
         </tbody>
       </table>
     </div>
 
     <div id="remarks-area">
       <div>備考:</div>
-      <div id="remarks">これは備考です。</div>
+      <div id="remarks">{{ $estimate->remarks }}</div>
     </div>
 </body>
 </html>
